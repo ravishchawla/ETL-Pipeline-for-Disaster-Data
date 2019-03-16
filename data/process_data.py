03c660cff66d7a8698365082299c47ad2661d0a2
Ravish Chawla <chawlar@vmware.com>
2019-03-15 22:50:39 -0400

    feat: Added completed code and jupyter notebooks
    Added completed code for the notebooks and
    added them to the scripts.

---
+++ data/process_data.py
@@ -0,0 +1,59 @@
+import sys
+import pandas as pd;
+import numpy as np;
+from sqlalchemy import create_engine;
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+
+    categories = pd.read_csv(categories_filepath)
+    
+    df = messages.merge(categories, on='id')
+    
+    return df;
+
+def clean_data(df):
+    categories = df['categories'].str.split(';', expand=True)
+    category_colnames = [x[:-2] for x in categories.iloc[0].values];
+    categories.columns = category_colnames;
+    categories = categories.apply(lambda x: x.str.replace(r"([a-z]+)(-|_)", ""));
+    categories = categories.astype('int')
+    
+    df = df.drop('categories', axis=1);
+    df = pd.concat([df, categories], axis=1)
+    df = df.drop_duplicates(keep='last');
+
+    return df;
+
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('disaster_data_cleaned', engine, index=False)
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,95 @@
+import sys
+# import libraries
+import pandas as pd;
+from sqlalchemy import create_engine;
+import re;
+from nltk import word_tokenize, pos_tag;
+from nltk.corpus import stopwords;
+from nltk.stem.wordnet import WordNetLemmatizer;
+from sklearn.pipeline import Pipeline;
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer;
+from sklearn.ensemble import RandomForestClassifier;
+from sklearn.multioutput import MultiOutputClassifier;
+from sklearn.metrics import accuracy_score, f1_score, classification_report, confusion_matrix;
+from sklearn.cross_validation import train_test_split;
+import seaborn as sns;
+import numpy as np;
+import nltk;
+import pickle;
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('punkt')
+
+def load_data(database_filepath):
+    engine = create_engine('sqlite:///' + database_filepath)
+    conn = engine.connect();
+    df = pd.read_sql('select * from disaster_data_cleaned', conn)
+    X = df['message']
+    Y = df.select_dtypes('int64').drop('id', axis=1)
+    
+    return X, Y, Y.columns;
+
+def tokenize(text):
+    word_net = WordNetLemmatizer();
+    
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower());
+    words = word_tokenize(text);
+    words = [word for word in words if word not in stopwords.words('english')];
+    lemmed = [word_net.lemmatize(word) for word in words];
+    return lemmed;
+
+def build_model():
+    forest = RandomForestClassifier(n_estimators=10, random_state=1024, criterion='gini', max_depth=5, max_features=5);
+    pipeline = Pipeline([('count', CountVectorizer(tokenizer=tokenize)),
+                         ('tfidf', TfidfTransformer()),
+                         ('model', MultiOutputClassifier(estimator=forest, n_jobs=1))
+                        ]);
+    return pipeline;
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_preds = model.predict(X_test);
+    Y_preds = pd.DataFrame(Y_preds);
+    Y_preds.columns = Y_test.columns;
+    Y_preds.index = Y_test.index;
+
+    for column in Y_test.columns:
+        print('Column : ' , column)
+        print(classification_report(Y_test[column], Y_preds[column]))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'));
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
